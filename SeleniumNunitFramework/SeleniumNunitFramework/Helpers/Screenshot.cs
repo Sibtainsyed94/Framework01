c1e1490f1c3ec9e23654a103b905d05a03514b9a
Salik Sadruddin Merani <salik.merani@gmail.com>
2021-01-21 00:47:49 +0500

    Screenshot is now part of its own helper class

---
+++ SeleniumNunitFramework/SeleniumNunitFramework/Helpers/Screenshot.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumNunitFramework.Helpers
+{
+  class Screenshot {
+    public static void TakeScreenshot(
+      ref IWebDriver driver,
+      string location,
+      string filename,
+      ScreenshotImageFormat format = ScreenshotImageFormat.Jpeg
+    ) {
+      var loc = string.Format("{0}{1}", location, filename);
+      ((ITakesScreenshot)driver).GetScreenshot().SaveAsFile(loc, ScreenshotImageFormat.Jpeg);
+    }
+  }
+}
