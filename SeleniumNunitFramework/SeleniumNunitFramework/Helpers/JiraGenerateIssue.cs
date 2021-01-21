95484fd0c6e72fe29f63d7b5727b1c1e10024cfe
Salik Sadruddin Merani <salik.merani@gmail.com>
2021-01-21 10:39:06 +0500

    Jira Should work now

--- SeleniumNunitFramework/SeleniumNunitFramework/Helpers/JiraGenerateIssue.cs
+++ SeleniumNunitFramework/SeleniumNunitFramework/Helpers/JiraGenerateIssue.cs
@@ -9,19 +9,14 @@ using System.Threading.Tasks;
 namespace SeleniumNunitFramework.Helpers
 {
   public class JiraGenerateIssue {
-  public  static void CreateIssue(
+    public  static void CreateIssue(
       string summary,
       List<string> attachments = null
     ) {
-            //var host = System.Configuration.ConfigurationManager.AppSettings["JiraHost"];
-            //var username = System.Configuration.ConfigurationManager.AppSettings["JiraUsername"];
-            //var password = System.Configuration.ConfigurationManager.AppSettings["JiraPassword"];
-            //var project = System.Configuration.ConfigurationManager.AppSettings["JiraProject"];
-            String host = "https://bqht.atlassian.net/";
-            String username = "sibtainsyed94@gmail.com";
-            String password = "colgatef00";
-            String project = "bqht";
-
+      var host = System.Configuration.ConfigurationManager.AppSettings["JiraHost"];
+      var username = System.Configuration.ConfigurationManager.AppSettings["JiraUsername"];
+      var password = System.Configuration.ConfigurationManager.AppSettings["JiraPassword"];
+      var project = System.Configuration.ConfigurationManager.AppSettings["JiraProject"];
       var jira = Jira.CreateRestClient(host, username, password);
       var issue = jira.CreateIssue(project);
       issue.Type = "Bug";
